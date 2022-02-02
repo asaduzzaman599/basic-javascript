c3222da37a0d5bd5b4ad52b5263d7851bd0a241f
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-03 01:33:44 +0600

    simple-mathematical operation

---
+++ simple-operation.js
@@ -0,0 +1,27 @@
+
+// Simple mathematical operation by using +, -, *, /, %
+
+let a = 12;
+let b = 5;
+
+let result = 0;
+
+//Addition   '+'
+result = a + b;                                 
+console.log("Resule: 12 + 5 =", result)             //Resule: 12 + 5 = 17
+
+//Subtraction   '-'
+result = a - b;
+console.log("Resule: 12 - 5 =", result)             //Resule: 12 - 5 = 7
+
+//Multiplication   '*'
+result = a * b;
+console.log("Resule: 12 * 5 =", result)             //Resule: 12 * 5 = 60
+
+//Division     '/'
+result = a / b;
+console.log("Resule: 12 / 5 =", result)             //Resule: 12 / 5 = 2.4
+
+//Modulus '%'
+result = a % b;
+console.log("Resule: 12 % 5 =", result)             //Resule: 12 % 5 =  2
