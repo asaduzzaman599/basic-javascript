b7a48f49fc69fa33a848ca93a013c38c9136b600
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-03 01:53:47 +0600

    comparison operation

--- comparison.js
+++ comparison.js
@@ -3,22 +3,32 @@
 var largeVariable = 10;                                     
 var smallVariable = 5;  
                                                      //output
-//largeVariable getter then smallerVariable                                                        
+//10 getter then 5                                                        
 console.log(largeVariable > smallVariable)           //true
 
-//largeVariable smaller then smallerVariable                
+//10 smaller then 5                
 console.log(largeVariable < smallVariable)           //false
 
-//largeVariable smaller or eqaul then smallerVariable                
+//10 smaller or eqaul then 5                
 console.log(largeVariable <= smallVariable)          //false
 
-//largeVariable getter or equal then smallerVariable                
+//10 getter or equal then 5                
 console.log(largeVariable >= smallVariable)          //true
 
 
-//largeVariable equal to smallerVariable                
+//10 equal to 5                
 console.log(largeVariable == smallVariable)         //false
 
-//largeVariable not equal to smallerVariable                
+//10 not equal to 5                
 console.log(largeVariable != smallVariable)         //true
+
+//5 divisible by 5 and 10 divisible by 3  
+//  '&&' any one condition false will condition false           
+console.log(smallVariable % 5 == 0 && largeVariable % 3 == 0 )                   //false
+
+
+//10 not equal to 5 or 5 getter then 10   
+//  '||' any one condition true will condition true         
+console.log(largeVariable != smallVariable || smallVariable > largeVariable )    //true
+
 
