d1fdb8fa4bfaec9ff216152d0459846082bdebca
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-03 03:20:59 +0600

    array condition forloop

---
+++ array-condition.js
@@ -0,0 +1,10 @@
+//you have array of numbers. You have to display number which are larger than 40
+
+let numbers = [1,55,8,40,68,1,21,99,50,35,400];
+
+for(const number of numbers){             //55 //40 //68 //99 //50 //400
+    if(number >=40){
+        console.log(number);
+    }
+}
+console.log('Array finished');             //Array finished
